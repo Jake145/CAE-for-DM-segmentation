94beafec013fa5761538fe1f693e218b3fd6997c
Jake145 <jakeharold.pensavalle@gmail.com>
2021-02-19 20:42:58 +0100

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
-    "sphinx.ext.mathjax",
+    "sphinx.ext.mathjax",'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
